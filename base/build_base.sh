8869cc49d31b314d0787788cf215935f94a9c390
herter4171 <nonyabusiness@nothing.org>
2018-11-22 05:25:25 -0500

    Docker centos install of moose build stack via bind mount

---
+++ base/build_base.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+yum install gcc \
+	gcc-c++ \
+	make \
+	freeglut-devel\
+	m4 \
+	blas-devel \
+	lapack-devel \
+	gcc-gfortran \
+	libX11-devel \
+	vi \
+	git \
+	wget -y
+
---
+++ base/run_base.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /opt/moose
+yum install moose-environment_centos-7.5.1804_20181012_x86_64.rpm -y
+
